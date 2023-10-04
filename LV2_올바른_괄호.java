2c6ef9c7608dcfde098243ba095a5b88a183cf05
Brucee97 <o3o970101@gmail.com>
2023-10-04 14:08:30 +0900

    Create LV2_올바른_괄호.java

---
+++ LV2_올바른_괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                count++;
+            else {
+                count--;
+                if (count == -1)
+                    return false;
+            }
+        }
+        if (count != 0)
+            return false;
+        return answer;
+    }
+}
