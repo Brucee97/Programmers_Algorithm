77e3143386e038188003886db73080dc1d679591
Brucee97 <o3o970101@gmail.com>
2023-10-04 13:05:33 +0900

    LV1_나누어_떨어지는_숫자_배열

---
+++ LV1_나누어_덜어지는_숫자_배열.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0)
+                answer.add(arr[i]);
+        }
+
+        if (answer.size() == 0)
+            answer.add(-1);
+
+        return answer;
+    }
+}
